c469806f96922b264786b6c2d79df4e336914a1a
Adam Auskerin <adam.auskerin@gmail.com>
2023-07-28 11:42:46 +0100

    feat: Complete useState tutorial
    https://www.youtube.com/watch?v=O6P86uwfdR0&list=PLZlA0Gpn_vH8EtggFGERCwMY5u5hOjf-h&ab_channel=WebDevSimplified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    /*
+     * If we need to do some calculation to initialise the state, rather than a hardcoded value such as `0`, we can.
+     * However, we should avoid putting a function call directly inside `useState()` as the function will then be called
+     * on every render rather than just the first. Instead, we can pass it as an initialiser function or use a callback.
+     *
+     * @see https://react.dev/reference/react/useState#avoiding-recreating-the-initial-state
+     */
+    const [count, setCount] = useState(0);
+
+    function decrementCount() {
+        /*
+         * When updating state based on the previous state, it is best to use a callback as seen below as opposed to
+         * simply `setCount(count - 1)`.
+         *
+         * @see https://react.dev/reference/react/useState#updating-state-based-on-the-previous-state
+         */
+        setCount(prevCount => prevCount - 1);
+    }
+
+    function incrementCount() {
+        setCount(prevCount => prevCount + 1);
+    }
+
+    return (
+        <div id="counter">
+            <button onClick={decrementCount}>-</button>
+            <span>{count}</span>
+            <button onClick={incrementCount}>+</button>
+        </div>
+    );
 }
 
 export default App;
